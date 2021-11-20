d41c92e4f7bb8738b813fefb064532498f60de28
Gordian Bruns <gordob1@web.de>
2021-11-20 17:07:15 -0500

    added courses scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :courses
   get 'home/display'
+  root 'home#display'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
