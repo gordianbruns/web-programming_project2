d9a11a068eccfb6f8f5c0e30bcee0c67babbc37a
Gordian Bruns <gordob1@web.de>
2021-11-22 14:11:31 -0500

    Added professor scaffold

---
+++ app/views/professors/_professor.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! professor, :id, :name, :title, :email, :created_at, :updated_at
+json.url professor_url(professor, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_204445) do
+ActiveRecord::Schema.define(version: 2021_11_22_190817) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 2021_11_21_204445) do
 
   create_table "departments", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "professors", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
